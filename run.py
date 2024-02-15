b20def1a67e6a407c5534e43dc54d66775c6622a
Tadgh Mc Carthy <tadghmccarthy618@gmail.com>
2024-02-15 13:59:05 +0000

    added validation

--- run.py
+++ run.py
@@ -12,8 +12,34 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
-sales = SHEET.worksheet('sales')
 
-data = sales.get_all_values()
+def get_sales_data():
+    """
+    Get sales figures input from user
+    """
+    print("Please enter sales data from last market")
+    print("Data should be six numbers , seperated by commas.")
+    print("Example: 10,20,30,40,50,60\n")
 
-print(data)
+    data_str = input("Enter your data here: ")
+    
+    sales_data = data_str.split(",")
+    print(sales_data)
+
+def validate_data(values):
+    """
+    inside the try,converts strings value to integers.
+    Raises ValueError if strings cannot be converted into int, 
+    or if there arent exactly 6 inputs
+    """ 
+    try:
+        if len(value) != 6:
+            raise ValueError(
+                f"6 values required, you only provided {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
+
+
+
+get_sales_data()
