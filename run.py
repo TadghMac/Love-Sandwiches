efcd2ca2897cfcd14f60233cea8b98e5b0d9bbb9
Tadgh Mc Carthy <tadghmccarthy618@gmail.com>
2024-02-16 10:31:16 +0000

    surplus data

--- run.py
+++ run.py
@@ -65,6 +65,16 @@ def update_sales_worksheet(data):
     sales_worksheet.append_row(data)
     print("Sales worksheet updated successfully.\n")
 
+def update_surplus_worksheet(data):
+    """
+    Update surplus worksheet, add new row with the list data provided
+    """
+    print("Updating surplus worksheet...\n")
+    surplus_worksheet = SHEET.worksheet("surplus")
+    surplus_worksheet.append_row(data)
+    print("Surplus worksheet updated successfully.\n")
+
+
 def calculate_surplus_data(sales_row):
 
     """
@@ -78,19 +88,19 @@ def calculate_surplus_data(sales_row):
     surplus_data=[]
     for stock,sales in zip(stock_row, sales_row):
         surplus = int(stock) - sales
-        surplus_data,append(surplus)
+        surplus_data.append(surplus)
     
     return surplus_data
 
 def main():
     """
-    Run all program function
+    Run all program functions
     """
     data = get_sales_data()
     sales_data = [int(num) for num in data]
     update_sales_worksheet(sales_data)
     new_surplus_data = calculate_surplus_data(sales_data)
-    print(new_surplus_data)
+    update_surplus_worksheet(new_surplus_data)
 
 print("Failte go dti love Sandwiches Data Automation")  
 main()  
